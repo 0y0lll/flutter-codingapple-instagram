492e5cec172a4031f7262a0763bbe15c70cac3ed
oyo <bemyangel2@gmail.com>
2023-09-20 13:34:38 +0900

    feat: grid, notification 적용

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'dart:io';
+import 'package:flutter_codingapple_instagram/utils/notification.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 import 'package:flutter_codingapple_instagram/style/style.dart' as style;
@@ -8,8 +9,10 @@ import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
-  runApp(ChangeNotifierProvider(
-    create: (context) => Store(),
+  runApp(MultiProvider(
+    providers: [
+      ChangeNotifierProvider(create: (_) => Store()),
+    ],
     child: MaterialApp(theme: style.themeData, initialRoute: '/', routes: {
       '/': (context) => MyApp(),
       // '/upload': (context) => Upload(),
@@ -33,6 +36,7 @@ class _MyAppState extends State<MyApp> {
   @override
   void initState() {
     super.initState();
+    initNotification();
 
     WidgetsBinding.instance.addPostFrameCallback((_) {
       getData();
@@ -51,6 +55,9 @@ class _MyAppState extends State<MyApp> {
     });
 
     return Scaffold(
+        floatingActionButton: FloatingActionButton(
+            onPressed: () => showNotification(),
+            child: Icon(Icons.notifications)),
         appBar: AppBar(
           title: Text('Instagram',
               style: TextStyle(
@@ -235,6 +242,7 @@ class _MyAppState extends State<MyApp> {
 
 class Upload extends StatelessWidget {
   Upload({Key? key, this.userImage, this.setUserContent}) : super(key: key);
+
   final userImage;
   final setUserContent;
   var textController = TextEditingController();
@@ -279,6 +287,8 @@ class Profile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    context.read<Store>().getProfile();
+
     return Scaffold(
         appBar: AppBar(
           title: Text(context.watch<Store>().name,
@@ -288,28 +298,22 @@ class Profile extends StatelessWidget {
                   letterSpacing: -1.5,
                   color: Colors.black)),
         ),
-        body: Column(
-          children: [
-            Text('Profile'),
-            TextButton(
-              onPressed: () {
-                context.read<Store>().changeName();
+        body: CustomScrollView(
+          slivers: [
+            SliverToBoxAdapter(
+                child:
+                    Padding(padding: EdgeInsets.all(16), child: UserProfile())),
+            SliverGrid.builder(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 3, //1 개의 행에 보여줄 item 개수
+                mainAxisSpacing: 1, //수평 Padding
+                crossAxisSpacing: 1, //수직 Padding
+              ),
+              itemBuilder: (BuildContext context, int index) {
+                return Image.network(context.watch<Store>().profiles[index]);
               },
-              child: Text('이름 변경',
-                  style: TextStyle(
-                    color: Colors.white,
-                  )),
-            ),
-            Text(context.watch<Store>().follower.toString()),
-            TextButton(
-              onPressed: () {
-                // TODO:
-              },
-              child: Text('팔로우',
-                  style: TextStyle(
-                    color: Colors.white,
-                  )),
-            ),
+              itemCount: context.watch<Store>().profiles.length,
+            )
           ],
         ));
   }
@@ -317,10 +321,66 @@ class Profile extends StatelessWidget {
 
 class Store extends ChangeNotifier {
   var name = 'leona';
-  var follower = 0;
+  var followers = 0;
+  var isFollow = false;
+  List profiles = [];
+
+  Future getProfile() async {
+    var result = await http
+        .get(Uri.parse('https://codingapple1.github.io/app/profile.json'));
+    var result2 = jsonDecode(result.body);
+
+    profiles = result2;
+    notifyListeners();
+  }
 
   void changeName() {
     name = 'Leona';
     notifyListeners();
+  }
+
+  void userFollow() {
+    isFollow = !isFollow;
+
+    if (!isFollow == true) {
+      if (followers > 0) {
+        followers--;
+      } else {
+        followers = 0;
+      }
+    } else {
+      followers++;
+    }
+
+    notifyListeners();
+  }
+}
+
+class UserProfile extends StatelessWidget {
+  const UserProfile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+      CircleAvatar(
+          radius: 30,
+          backgroundColor: Colors.black12,
+          backgroundImage: AssetImage('assets/images/exampleProfile1.jpg')),
+      Text('followers ${context.watch<Store>().followers.toString()}'),
+      TextButton(
+        onPressed: () {
+          context.read<Store>().userFollow();
+        },
+        style: ButtonStyle(
+            backgroundColor: context.watch<Store>().isFollow
+                ? MaterialStateProperty.all(Colors.black26)
+                : MaterialStateProperty.all(Colors.black)),
+        child: Text(context.watch<Store>().isFollow ? '팔로잉' : '팔로우',
+            style: TextStyle(
+              color:
+                  context.watch<Store>().isFollow ? Colors.black : Colors.white,
+            )),
+      ),
+    ]);
   }
 }
